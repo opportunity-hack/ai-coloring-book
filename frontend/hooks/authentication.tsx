38192e8060097390e4cb83f67b72971fe4d9a107
Darshan <dashbuis@gmail.com>
2024-02-06 16:26:02 -0700

    remove existing code

--- frontend/hooks/authentication.tsx
+++
@@ -1,9 +0,0 @@
-
-import React, { useState, useEffect } from 'react'
-export const getToken = () => {
-    // get token from local storage
-    const token = "localStorage.getItem('suzieToken')"
-    return {
-        token: token
-    };
-}
